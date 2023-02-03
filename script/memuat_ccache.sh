deabefa3b0550343b215375ff08a3db78d274a48
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-02-03 23:14:12 +0700

    build [skip ci]

--- script/memuat_ccache.sh
+++ script/memuat_ccache.sh
@@ -8,7 +8,7 @@ echo "$RCLONECONFIG_DRIVE" > ~/.config/rclone/rclone.conf
 name_rom=$(grep init $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d / -f 4)
 nama_perangkat=$(grep unch $CIRRUS_WORKING_DIR/build.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)
 
-rclone copy --drive-chunk-size 256M --stats 1s mobx:ccache/$nama_perangkat/$name_rom/ccache.tar.gz $WORKDIR -P
+rclone copy --drive-chunk-size 256M --stats 1s build:ccache/$nama_perangkat/$name_rom/ccache.tar.gz $WORKDIR -P
 
 tar xzf ccache.tar.gz
 rm -rf ccache.tar.gz
