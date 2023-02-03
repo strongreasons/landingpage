a5e3cfff74b017ce80f314a2d3db94d7a4aea12f
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-02-03 23:15:40 +0700

    build [skip ci]

--- script/mengemas.sh
+++ script/mengemas.sh
@@ -70,7 +70,7 @@ function upload_ccache() {
         tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
     }
     time compress ccache 1
-    rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz mobx:ccache/$perangkat/$nama_rom -P
+    rclone copy --drive-chunk-size 256M --stats 1s ccache.tar.gz build:ccache/$perangkat/$nama_rom -P
     rm -rf ccache.tar.gz
     pesan Mengunggah ccache berhasil...
 }
