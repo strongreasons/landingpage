9baa28772cf2c8f7b555a0d6019562201276da40
strongreasons <109900777+strongreasons@users.noreply.github.com>
2023-02-04 10:58:18 +0700

    Sss

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 source build/envsetup.sh
-export TZ=Asia/Jakarta
+export TZ=Asia/Shanghai
 export KBUILD_BUILD_USER=$KBUILD_BUILD_USER
 export KBUILD_BUILD_HOST=$KBUILD_BUILD_HOST
 export BUILD_USERNAME=$KBUILD_BUILD_USER
